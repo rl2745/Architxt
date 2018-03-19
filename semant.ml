2a43afebfa84b0229ce5c709da956e97c78cfa09
jni1996 <jni21519@gmail.com>
2018-03-19 13:44:12 -0400

    added global check and some built in functions to semant.ml

---
+++ semant.ml
@@ -0,0 +1,52 @@
+(*semantic checking for architxt compiler based on microc compiler
+returns SAST if successful, throws an exception if something goes wrong*)
+
+open Ast
+open Sast
+
+module StringMap = Map.Make(String)
+
+let check (globals, functions) = 
+	(*check binds used for globals, formals, and locals as well*)
+	let check_binds (kind: string) (to_check: bind list) = 
+		let check_it checked binding = 
+			let void_err = "illegal void " ^kind ^ " " ^snd binding
+			and dup_err = "duplicate " ^ kind ^ " " ^snd binding
+			in match binding with
+        		(* No void bindings *)
+        		(Void, _) -> raise (Failure void_err)
+      		| (_, n1) -> match checked with
+                    (* No duplicate bindings *)
+                      ((_, n2) :: _) when n1 = n2 -> raise (Failure dup_err)
+                    | _ -> binding :: checked
+    in let _ = List.fold_left check_it [] (List.sort compare to_check) 
+       in to_check
+  in 
+
+  (**** Checking Global Variables ****)
+
+  let globals' = check_binds "global" globals in
+
+  (* Collect function declarations for built-in functions: no bodies 
+  print (for string and int), fill, propagate, display*)
+  let built_in_decls = 
+  	StringMap.add "print_i" { typ = Void; fname = "print_i"; formals = [(Int, "x")];
+      locals = []; body = [] } 
+    ( StringMap.add "print" { typ = Void; fname = "print"; formals = [(String, "x")];
+      locals = []; body = [] })
+    ( StringMap.add "fill" { typ = Void; fname = "fill"; formals = [(Int, "x"); (Int, "y"); (Point, "p")];
+      locals = []; body = [] }
+    ( StringMap.add "propagate" { typ = Void; fname = "propagate"; formals = [(Point, "p")];
+      locals = []; body = [] }
+    ( StringMap.add "display" { typ = Void; fname = "display"; formals = [(Map, "m")];
+      locals = []; body = [] }
+  in
+
+  (*add function name to symbol table*)
+  (*collect function names into one symbol table*)
+  (*return function from symbol table*)
+  (*checkbinds on formals and locals to make sure formals or locals are not void/dup*)
+  (*build local symbol table of variables for function*)
+  (*return local variable from local symbol table*)
+  (*return semantically checked expression with a type *)
+  (*return semantically checked statement with sexprs*)
