e2e3e8dfdafc20379943792873367f582e63e075
Richard Lopez <richard.lopez@columbia.edu>
2018-03-25 11:28:30 -0400

    added hello world file and updated codegen with some functionality for printing strings

--- codegen.ml
+++ codegen.ml
@@ -26,23 +26,26 @@ module StringMap = Map.Make(String)
 let translate (_, functions) =
   let context    = L.global_context () in
   (* Add types to the context so we can use them in our LLVM code *)
-  let i32_t      = L.i32_type    context
+  let the_module = L.create_module context "Architxt"
+  and i32_t      = L.i32_type    context
   and i8_t       = L.i8_type     context 
-  and void_t     = L.void_type   context 
+  and void_t     = L.void_type   context in
+  let str_t  = L.pointer_type i8_t in
   (* Create an LLVM module -- this is a "container" into which we'll 
      generate actual code *)
-  and the_module = L.create_module context "MicroC" in
+  
 
-  (* Convert MicroC types to LLVM types *)
+  (* Convert Architxt types to LLVM types *)
   let ltype_of_typ = function
       A.Int   -> i32_t
     | A.Void  -> void_t
+    | A.String -> str_t
     | t -> raise (Failure ("Type " ^ A.string_of_typ t ^ " not implemented yet"))
   in
 
   (* Declare a "printf" function to implement MicroC's "print". *)
   let printf_t : L.lltype = 
-      L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+      L.var_arg_function_type srt_t [| L.pointer_type str_t |] in
   let printf_func : L.llvalue = 
      L.declare_function "printf" printf_t the_module in 
 
@@ -58,12 +61,16 @@ let translate (_, functions) =
     let builder = L.builder_at_end context (L.entry_block the_function) in
     (* Create a pointer to a format string for printf *)
     let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
+    let str_format_str = L.build_global_stringptr "%s\n" "fmt" builder in
     (* Generate LLVM code for a call to MicroC's "print" *)
     let rec expr builder ((_, e) : sexpr) = match e with
 	SLiteral i -> L.const_int i32_t i (* Generate a constant integer *)
-      | SCall ("print", [e]) -> (* Generate a call instruction *)
-	  L.build_call printf_func [| int_format_str ; (expr builder e) |]
+      | SCall ("print_i", [e]) -> (* Generate a call instruction *)
+	       L.build_call printf_func [| int_format_str ; (expr builder e) |]
 	    "printf" builder 
+      | SCall ("print", [e]) ->
+          L.build_call printf_func [| str_format_str ; (expr builder e) |]
+            "printf" builder
       (* Throw an error for any other expressions *)
       | _ -> to_imp (string_of_sexpr (A.Int,e))  
     in
---
+++ helloworld.arc
@@ -0,0 +1,8 @@
+#Hello World to Test Code Gen
+
+int main() {
+	String helloWorld = "Hello World!";
+	printf(helloWorld);
+	return 0;
+}
+ 
