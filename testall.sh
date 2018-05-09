6463f3bd1f04496fa99a9e5ac6258723e9b01435
Richard Lopez <richard.lopez@columbia.edu>
2018-05-09 10:43:37 -0400

    added display c file obj to testall file

--- testall.sh
+++ testall.sh
@@ -94,7 +94,7 @@ Check() {
     generatedfiles="$generatedfiles ${basename}.ll ${basename}.s ${basename}.exe ${basename}.out" &&
     Run "$ARCHITXT" "$1" ">" "${basename}.ll" &&
     Run "$LLC" "${basename}.ll" ">" "${basename}.s" &&
-    Run "$CC" "-o" "${basename}.exe" "${basename}.s" &&
+    Run "$CC" "-o" "${basename}.exe" "${basename}.s" "display.o" &&
     Run "./${basename}.exe" > "${basename}.out" &&
     Compare ${basename}.out ${reffile}.out ${basename}.diff
 
@@ -165,12 +165,12 @@ LLIFail() {
 
 which "$LLI" >> $globallog || LLIFail
 
-#if [ ! -f printbig.o ]
-#then
-#    echo "Could not find printbig.o"
-#    echo "Try \"make printbig.o\""
-#    exit 1
-#fi
+if [ ! -f display.o ]
+then
+    echo "Could not find display.o"
+    echo "Try \"make display.o\""
+    exit 1
+fi
 
 if [ $# -ge 1 ]
 then
